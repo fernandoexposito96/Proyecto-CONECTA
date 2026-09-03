4ce4901238c6944c2e4b9beab61ddcd30cb6658c
fernandoexposito96 <fernando.exposito.96@hotmail.com>
2026-09-03 15:43:37 +0200

    Stage optimized data layer integration

---
+++ src/data/optimizedDataLayer.ts
@@ -0,0 +1,15 @@
+export { loadConectaData } from "./loadConectaData";
+export {
+  refreshPlanMembers,
+  refreshSavedItems,
+  refreshConnections,
+  refreshConversations,
+} from "./refreshConectaSlices";
+export {
+  joinPlanMutation,
+  leavePlanMutation,
+  savePlanMutation,
+  unsavePlanMutation,
+  connectMutation,
+  blockConnectionMutation,
+} from "./conectaMutations";

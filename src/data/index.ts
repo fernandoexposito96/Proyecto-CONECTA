c401469030f52accbc0f2e7d251609330ccb6c20
fernandoexposito96 <fernando.exposito.96@hotmail.com>
2026-09-03 15:44:23 +0200

    Expose optimized CONECTA data API

---
+++ src/data/index.ts
@@ -0,0 +1,3 @@
+export { loadConectaData } from "./loadConectaData";
+export { createOptimizedActions } from "./optimizedDataLayer.integration";
+export type { OptimizedStateSetters } from "./optimizedDataLayer.integration";

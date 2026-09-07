c45d8fe4f3f67b84e482e111c91f41c2a234e106
fernandoexposito96 <fernando.exposito.96@hotmail.com>
2026-09-07 22:08:04 +0200

    backend: configure Supabase client

---
+++ src/lib/supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl='https://qdjuypoqiafqncwgmicf.supabase.co';
+const supabasePublishableKey='sb_publishable_cJAhkB9oDjK-ecmrEshNvA_79F3_G55';
+
+export const supabase=createClient(supabaseUrl,supabasePublishableKey,{
+  auth:{
+    persistSession:true,
+    autoRefreshToken:true,
+    detectSessionInUrl:true,
+  },
+});
